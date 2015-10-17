0ee624c7bdf4c129bf4d6deec3bcaf984f914807
maebeale <maebeale@gmail.com>
2015-10-16 21:51:22 -0400

    WIP: code club solution using modules & constants

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,50 +1,99 @@
-def update_quality(items)
-  items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
+module Updater
+  def update
+    self.quality -= 1 unless [0, 50].include?(self.quality)
+    self.sell_in -= 1
+  end
+end
+
+module AgedBrie
+  def quality=(_arg)
+    self[:quality] += 1
+  end
+end
+
+module BackstagePasses
+  def quality=(_arg)
+    return 0 if self.sell_in == 0
+    if self.sell_in <= 5
+      self[:quality] += 3
+    elsif self.sell_in <= 10
+      self[:quality] += 2
     else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
+      self[:quality] += 1
     end
   end
 end
+
+module Sulfuras
+  def quality=(_arg)
+    DEFAULT_SULFURAS_QUALITY
+  end
+
+  def sell_in=(_arg)
+    DEFAULT_SULFURAS_QUALITY
+  end
+end
+
+item_hash = {
+  AGED_BRIE => AgedBrie,
+  BACKSTAGE_PASSES => BackstagePasses,
+  SULFURAS => Sulfuras
+}
+
+DEFAULT_SULFURAS_QUALITY = 80.freeze
+AGED_BRIE = 'Aged Brie'
+BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert'
+SULFURAS = 'Sulfuras, Hand of Ragnaros'
+
+def update_quality(items)
+  items.each do |item|
+    item.extend(item_hash[item.name], Updater)
+    item.update
+  end
+end
+#
+#     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+#       if item.quality > 0
+#         if item.name != 'Sulfuras, Hand of Ragnaros'
+#           item.quality -= 1
+#         end
+#       end
+#     else
+#       if item.quality < 50
+#         item.quality += 1
+#         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+#           if item.sell_in < 11
+#             if item.quality < 50
+#               item.quality += 1
+#             end
+#           end
+#           if item.sell_in < 6
+#             if item.quality < 50
+#               item.quality += 1
+#             end
+#           end
+#         end
+#       end
+#     end
+#     if item.sell_in < 0
+#       if item.name != "Aged Brie"
+#         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+#           if item.quality > 0
+#             if item.name != 'Sulfuras, Hand of Ragnaros'
+#               item.quality -= 1
+#             end
+#           end
+#         else
+#           item.quality = item.quality - item.quality
+#         end
+#       else
+#         if item.quality < 50
+#           item.quality += 1
+#         end
+#       end
+#     end
+#   end
+# end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
@@ -61,3 +110,4 @@ Item = Struct.new(:name, :sell_in, :quality)
 #   Item.new("Conjured Mana Cake", 3, 6),
 # ]
 
+# Item.new("Aged Brie", 2, 0)
